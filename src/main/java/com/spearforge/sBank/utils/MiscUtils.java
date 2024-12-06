452329e34d1eef697d82c9c0ddfe18734783cee6
Niix <46878004+Niix-Dan@users.noreply.github.com>
2024-12-06 02:11:13 -0300

    Update MiscUtils.java

--- src/main/java/com/spearforge/sBank/utils/MiscUtils.java
+++ src/main/java/com/spearforge/sBank/utils/MiscUtils.java
@@ -1,10 +1,27 @@
 package com.spearforge.sBank.utils;
 
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
 
+import java.util.Optional;
+import java.util.regex.Pattern;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 
 public class MiscUtils {
+
+    public static double getInterest(Player player) {
+        Pattern pattern = Pattern.compile("^sbank\\.interest\\.(\\d+)$"); // sbank.interest.%
+
+        return player.getEffectivePermissions().stream()
+                .map(PermissionAttachmentInfo::getPermission)
+                .filter(permission -> pattern.matcher(permission).matches())
+                .map(permission -> pattern.matcher(permission))
+                .filter(Matcher::find)
+                .mapToInt(matcher -> Integer.parseInt(matcher.group(1)))
+                .max()
+                .orElse(0);
+    }
 
     public static String formatBalance(double balance) {
         DecimalFormatSymbols symbols = new DecimalFormatSymbols();
